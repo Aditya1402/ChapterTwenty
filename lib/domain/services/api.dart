6238bedbdf7f6ecb7f9de6581083eb5eb4e87e6d
Aditya Patil <75464869+Aditya1402@users.noreply.github.com>
2022-08-01 09:51:33 +0530

    Add ReadMe images

--- lib/domain/services/api.dart
+++ lib/domain/services/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static String url = "https://www.googleapis.com/books/v1/volumes?q=flutter";
+  static String url = "https://www.googleapis.com/books/v1/volumes";
 
   static Future<List<dynamic>> fetchBooks() async {
     var result = await http.get(Uri.parse(url));
